4032cc31daab75f766606acc935126ef2eab261b
MTRNord <mtrnord1@gmail.com>
2014-07-08 15:42:52 +0200

    add config writing for mysql in installer script

---
+++ themes/accentbox/index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html crossriderapp2258="true" class=" js flexbox no-touch rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio" dir="ltr" lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+	
+	<title>Accentbox Theme Demo</title>
+		
+	<!--iOS/android/handheld specific -->	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">						
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+	<link rel="stylesheet" type="text/css" media="all" href="css/style.css">
+
+			<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<script src="js/modernizr.js"></script>
+	<script src="js/customscript.js" type="text/javascript"></script>
+	
+	<style type="text/css">
+					#header h1, #header h2 {
+			text-indent: -999em;
+			min-width:200px; margin-top: 0;
+			}
+			#header h1 a, #header h2 a{
+			background: url(images/logo.png) no-repeat;
+			min-width: 200px;
+			display: block;
+			min-height: 80px;
+			line-height: 28px;
+			}
+							.more a, .bubble a:hover, #commentform input#submit {
+				background-color: #79ACCD;
+			}
+			a, .title a:hover, #navigation ul ul li a:hover, #navigation > ul > li > a:hover {
+			color:#79ACCD;
+			}
+							</style>
+
+	</head>
+
+	<body>
+		
+		<header class="main-header">
+			<div class="container">
+				<div id="header">
+					<h1 id="logo">
+						<a href="#">Accentbox WordPress Theme Demo</a>
+					</h1><!-- END #logo -->
+					<div class="widget-area widget-header">	<ul><div class="textwidget"></div></ul></div>				
+				</div><!--#header-->
+			</div><!--.container-->
+		</header>
+		
+		<div class="container">
+			<div class="secondary-navigation">
+				<nav id="navigation">
+					<ul class="menu"><li class="menu-item"><a href="#">Home</a></li>
+						<li class="menu-item"><a href="#">About</a></li>
+						<li class="menu-item"><a href="#">Contact Us</a></li>
+						<li class="menu-item"><a href="#">Awesomeness</a></li>
+						<li class="menu-item"><a href="#">featured</a></li>
+						<li class="menu-item"><a href="#">Brochure</a></li>
+						<li class="menu-item"><a href="#">Design</a></li>
+						<li class="menu-item"><a href="#">Inspiration</a></li>
+					</ul>									
+				<select><option value="" selected="selected">Go to...</option><option value="#">Home</option><option value="#">About</option><option value="#">Contact Us</option><option value="#">Awesomeness</option><option value="#">featured</option><option value="#">Brochure</option><option value="#">Design</option><option value="#">Inspiration</option></select></nav>
+			</div>
+		</div>
+		
+		<div id="page">
+	<div class="content">
+		<article class="article">
+			<div id="content_box">
+			
+				<div class="post excerpt ">
+					<header>
+					<div class="bubble"><a href="#">4</a></div>
+						<h2 class="title">
+							<a href="#" rel="bookmark">Porttitor Lectus Tincidunt Elementum Nascetur Montes</a>
+						</h2>
+						<div class="post-info">
+							<span class="theauthor"><a href="#" rel="author">MTS</a></span>
+							<time>March 26, 2012</time>
+							<span class="thecategory"><a href="#" rel="category tag">Awesomeness</a></span>
+						</div>
+					</header><!--.header-->
+					<div class="post-content image-caption-format-1">
+						<a href="#" rel="nofollow" id="featured-thumbnail">
+						 
+						<div class="featured-thumbnail"><img src="images/2457315858_32ffd98aec_n-150x150.jpg" class="attachment-featured wp-post-image" height="150" width="150"><div>															</a>
+						<p>Nascetur, vel massa enim lundium nec in, phasellus vel nisi 
+						nec penatibus? Duis phasellus, ut! Amet risus ultricies integer! 
+						Elementum natoque! Nec, tincidunt risus adipiscing. Magna, augue 
+						dignissim in elementum diam ultricies. Enim, enim, sed augue nec? Montes
+						habitasse ac, ridiculus, placerat cras sed! Dis nunc? Adipiscing, porta
+						magna? Nunc non lundium? Vel, pulvinar aenean, </p>
+						<p class="more"><a href="#">Read More…</a></p>
+					</div>
+				</div><!--.post excerpt-->
+											
+				<div class="post excerpt ">
+					<header>
+					<div class="bubble"><a href="#">0</a></div>
+						<h2 class="title">
+							<a href="#" rel="bookmark">Mauris Dictum Libero Id Justo Fusce In Est</a>
+						</h2>
+						<div class="post-info">
+							<span class="theauthor"><a href="#" rel="author">MTS</a></span>
+							<time>September 17, 2008</time>
+							<span class="thecategory"><a href="#" rel="category tag">Awesomeness</a>, <a href="#" rel="category tag">featured</a></span>
+						</div>
+					</header><!--.header-->
+					<div class="post-content image-caption-format-1">
+					<a href="#" rel="nofollow" id="featured-thumbnail">
+						 
+						<div class="featured-thumbnail"><img src="images/6585158069_29788f608e_n-150x150.jpg" class="attachment-featured wp-post-image" height="150" width="150"></div>															</a>
+						<p>Nulla sagittis convallis arcu. Sed sed nunc. Curabitur 
+						consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et,
+						nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce
+						in est. Sed nec diam. Pellentesque habitant morbi tristique senectus et
+						netus et malesuada fames ac turpis egestas. Quisque semper nibh eget 
+						nibh. Sed tempor. </p>
+						<p class="more"><a href="#">Read More…</a>
+					</p>
+					</div>
+				</div><!--.post excerpt-->
+											
+				<div class="post excerpt last">
+					<header>
+					<div class="bubble"><a href="#">0</a></div>
+						<h2 class="title">
+							<a href="#" rel="bookmark">Cum Sociis Natoque Penatibus Magnis</a>
+						</h2>
+						<div class="post-info">
+							<span class="theauthor"><a href="#" rel="author">MTS</a></span>
+							<time>September 17, 2008</time>
+							<span class="thecategory"><a href="#" rel="category tag">Child Category III</a>, <a href="#" rel="category tag">featured</a>, <a href="#" rel="category tag">Parent Category III</a></span>
+						</div>
+					</header><!--.header-->
+					<div class="post-content image-caption-format-1">
+					<a href="#" rel="nofollow" id="featured-thumbnail">
+						 
+						<div class="featured-thumbnail"><img src="images/44066633_e0ab44de09_n-150x150.jpg" class="attachment-featured wp-post-image" height="150" width="150"></div>															</a>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed
+						eleifend urna eu sapien. Quisque posuere nunc eu massa. Praesent 
+						bibendum lorem non leo. Morbi volutpat, urna eu fermentum rutrum, ligula
+						lacus interdum mauris, ac pulvinar libero pede a enim. Etiam commodo 
+						malesuada ante. Donec nec ligula. Curabitur mollis semper diam. Duis 
+						viverra nibh a felis </p>
+						<p class="more"><a href="#">Read More…</a>
+											</p>
+					</div>
+				</div><!--.post excerpt-->
+											
+				<div class="post excerpt ">
+					<header>
+					<div class="bubble"><a href="#">2</a></div>
+						<h2 class="title">
+							<a href="#" rel="bookmark">Aliquet Pid Massa Porttitor Dictumst Ac Aenean A Dolor?</a>
+						</h2>
+						<div class="post-info">
+							<span class="theauthor"><a href="#" rel="author">MTS</a></span>
+							<time>September 11, 2008</time>
+							<span class="thecategory"><a href="#" rel="category tag">Awesomeness</a>, <a href="#" rel="category tag">featured</a></span>
+						</div>
+					</header><!--.header-->
+					<div class="post-content image-caption-format-1">
+					<a href="#" rel="nofollow" id="featured-thumbnail">
+						 
+						<div class="featured-thumbnail"><img src="images/2470289415_b98e0e1dae_n-150x150.jpg" class="attachment-featured wp-post-image" height="150" width="150"></div>															</a>
+						<p>Elit quis est sit integer rhoncus integer vel lorem mattis 
+						rhoncus natoque, cum. Sed, cras, sed? Dis tincidunt! Vel lacus 
+						elementum, in augue mus. Placerat, velit tristique et, magna tristique 
+						dolor magna, aliquam et elementum in elementum! Turpis placerat proin 
+						porta etiam augue adipiscing ridiculus. In, ridiculus nec aliquam nisi 
+						turpis rhoncus in tempor. A, </p>
+						<p class="more"><a href="#">Read More…</a>
+											</p>
+					</div>
+				</div><!--.post excerpt-->
+				
+				<div class="post excerpt ">
+					<header>
+					<div class="bubble"><a href="#">0</a></div>
+						<h2 class="title">
+							<a href="#" rel="bookmark">Quotes Time!</a>
+						</h2>
+						<div class="post-info">
+							<span class="theauthor"><a href="#" rel="author">MTS</a></span>
+							<time>September 11, 2008</time>
+							<span class="thecategory"><a href="#" rel="category tag">Awesomeness</a></span>
+						</div>
+					</header><!--.header-->
+					<div class="post-content image-caption-format-1">
+					<a href="#" rel="nofollow" id="featured-thumbnail">
+						 
+						<div class="featured-thumbnail"></div>															</a>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+						Curabitur quam augue, vehicula quis, tincidunt vel, varius vitae, nulla.
+						Sed convallis orci. Duis libero orci, pretium a, convallis quis, 
+						pellentesque a, dolor. Curabitur vitae nisi non dolor vestibulum 
+						consequat. Proin vestibulum. Ut ligula. Nullam sed dolor id odio 
+						volutpat pulvinar. Integer a leo. In et eros </p>
+						<p class="more"><a href="#">Read More…</a>
+					</p>
+					</div>
+				</div><!--.post excerpt-->
+				
+				<div class="post excerpt last">
+					<header>
+					<div class="bubble"><a href="#">0</a></div>
+						<h2 class="title">
+							<a href="#" rel="bookmark">A Post With A Left-Aligned Image</a>
+						</h2>
+						<div class="post-info">
+							<span class="theauthor"><a href="#" rel="author">MTS</a></span>
+							<time>September 11, 2008</time>
+							<span class="thecategory"><a href="#" rel="category tag">Parent Category II</a>, <a href="#" rel="category tag">Parent Category III</a></span>
+						</div>
+					</header><!--.header-->
+					<div class="post-content image-caption-format-1">
+					<a href="#" rel="nofollow" id="featured-thumbnail">
+						 
+						<div class="featured-thumbnail"><img src="images/3478158612_4a09ab2d3c-150x150.jpg" class="attachment-featured wp-post-image" height="150" width="150"></div>															</a>
+						<p>Nulla sagittis convallis arcu. Sed sed nunc. Curabitur 
+						consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et,
+						nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce
+						in est. Sed nec diam. Pellentesque habitant morbi tristique senectus et
+						netus et malesuada fames ac turpis egestas. Quisque semper nibh eget 
+						nibh. Sed tempor. </p>
+						<p class="more"><a href="#">Read More…</a>
+					</p>
+					</div>
+				</div><!--.post excerpt-->
+												
+				<div class="pnavigation2">
+					<div class="nav-previous left"><a href="#"><span class="meta-nav">←</span> Older posts</a></div>
+					<div class="nav-next right"></div>
+				</div>
+
+			</div>
+		</article>
+		
+		<aside class="sidebar c-4-12">
+			<div id="sidebars" class="g">
+				<div class="sidebar">
+					<ul class="sidebar_list">
+						<li class="widget widget-sidebar">
+							<form method="get" id="searchform" class="search-form" _lpchecked="1">
+							<fieldset> 
+								<input name="s" id="s" value="Search this Site..." type="text"> 
+								<input id="search-image" class="sbutton" src="images/search.png" style="border: 0px none; vertical-align: top;" type="image">
+							</fieldset>
+							</form>
+						</li>
+				
+						<li class="widget widget-sidebar"><div class="ad-300"><a href="#"><img src="images/stylishuserinterface300x250.jpg" height="250" width="300"></a></div></li>
+						<li class="widget widget-sidebar"><h3>Our Sponsors</h3><div class="ad-125"><ul><li class="oddad"><a href="#"><img src="images/125x125.gif" height="125" width="125"></a></li><li class="evenad"><a href="#"><img src="images/125x125.gif" height="125" width="125"></a></li></ul></div></li>		
+						<li class="widget widget-sidebar">		<h3>Recent Posts</h3>		<ul>
+								<li><a href="#">Porttitor Lectus Tincidunt Elementum Nascetur Montes</a></li>
+								<li><a href="#">Mauris Dictum Libero Id Justo Fusce In Est</a></li>
+								<li><a href="#">Cum Sociis Natoque Penatibus Magnis</a></li>
+								<li><a href="#">Aliquet Pid Massa Porttitor Dictumst Ac Aenean A Dolor?</a></li>
+								<li><a href="#">Quotes Time!</a></li>
+								</ul>
+						</li>
+						<li class="widget widget-sidebar">        	
+							<div class="mts-subscribe">
+								
+								<form style="" action="#" method="post" target="popupwindow" _lpchecked="1">
+								<input value="Your email Address..." type="text">
+								<input value="mythemeshop" name="uri" type="hidden"><input name="loc" value="en_US" type="hidden"><input value="Subscribe" type="submit">
+								</form>
+
+								<div class="result"></div>
+								
+							</div><!--subscribe_widget-->
+						
+						</li>
+					</ul>
+				</div>
+			</div><!--sidebars-->
+		</aside>	
+		
+	</div><!--#page-->
+</div><!--.container-->
+	<footer>
+		<div class="container">
+			<div class="footer-widgets">
+				<div class="f-widget">
+					<div class="widget"><h3>Lorem ipsum</h3>			
+						<div class="textwidget">Lorem
+						 ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur quam 
+						augue, vehicula quis, tincidunt vel, varius vitae, nulla. Sed convallis 
+						orci. Duis libero orci, pretium a.</div>
+					</div>	
+				</div>
+				<div class="f-widget">
+					<div class="widget"><h3>Consectetuer adipiscing</h3>			<div class="textwidget">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <a href="#">Curabitur</a> quam augue, vehicula quis, tincidunt vel, varius vitae, nulla. Sed convallis orci. Duis libero orci, pretium a.</div>
+					</div>		
+				</div>
+				<div class="f-widget last">
+					<div class="widget">		<h3>Recent Posts</h3>		<ul>
+						<li><a href="#">Porttitor Lectus Tincidunt Elementum Nascetur Montes</a></li>
+						<li><a href="#">Mauris Dictum Libero Id Justo Fusce In Est</a></li>
+						<li><a href="#">Cum Sociis Natoque Penatibus Magnis</a></li>
+						<li><a href="#">Aliquet Pid Massa Porttitor Dictumst Ac Aenean A Dolor?</a></li>
+						</ul>
+					</div>		
+				</div>
+				<div class="copyrights">
+					<div class="row" id="copyright-note">
+					<span>© 2012</span> - <span>Theme by <a href="http://mythemeshop.com/">MyThemeShop</a>.</span>
+					</div>
+					<div class="top"><a href="#">Back to Top ↑</a></div>
+				</div>
+			</div><!--.footer-widgets-->
+		</div><!--.container-->
+	</footer><!--footer-->
+
+</body></html>
