41cb97ddc892e504c63d254b348f8c403403632a
MTRNord <mtrnord1@gmail.com>
2014-11-17 17:17:53 +0100

    auto commit

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@
 git add . --all
 git commit -m "auto commit"
 git remote add origin git@github.com:RpicmsTeam/RPICMS.git
-git push -f -u origin develop --all
+git push -f -u origin develop
 sleep 30
