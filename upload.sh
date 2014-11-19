3d5be76117d3e9ba3ac2322287cafa331fec3d8c
MTRNord <mtrnord1@gmail.com>
2014-11-19 20:10:46 +0100

    $commit

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ echo "Commit:"
 read commit
 git commit -m '$commit'
 git remote add origin https://github.com/RpicmsTeam/RPICMS.git
-#git push -f -u origin develop
+git push -f -u origin develop
 git remote remove origin
 echo "Fertig!"
 sleep 10
