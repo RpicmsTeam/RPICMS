90d40274e13164dd8e0ac2366841471daecff45b
MTRNord <mtrnord1@gmail.com>
2014-11-19 20:11:46 +0100

    Reworked File Structure complete. Not all things will work!

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 git add . --all
 echo "Commit:"
 read commit
-git commit -m '$commit'
+git commit -m "$commit"
 git remote add origin https://github.com/RpicmsTeam/RPICMS.git
 git push -f -u origin develop
 git remote remove origin
