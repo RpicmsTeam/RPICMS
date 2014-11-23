739a5eb1b164a4b9c5e979fd6f0af0f244f87ff6
MTRNord <mtrnord1@gmail.com>
2014-11-23 11:29:41 +0100

    move debug code

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -77,9 +77,9 @@
       <div class="container">
             <h1>
               <?php
-              echo $post_id[$x];
               $x = 1;
               while ($x < $post_id_clean){
+                echo $post_id[$x];
                 echo "<a href='index.php?id=".$post_id[$x]."' rel='bookmark'> $post_title </a>";
                 $x = $x + 1;
               }
