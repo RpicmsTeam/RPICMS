d33c41937b2c12969d20b160bd01f2368370b3fb
MTRNord <mtrnord1@gmail.com>
2014-11-23 12:27:06 +0100

    rewrite database query

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -7,6 +7,7 @@
     # include required files #
     ##########################
     $empty = empty($_GET["id"]);
+    $id = $_GET["id"];
     include('../../core/backend/blog/posts.php');
     include('../../core/config/variables.config.php')
   ?>
