ebd962719fdfb705e688c0037262b184554722f0
MTRNord <mtrnord1@gmail.com>
2014-11-23 14:59:35 +0100

    try to fix endless loop -.- try 99999 or something like that :D

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -85,7 +85,7 @@
                 $x = 1;
                 if ($empty){
                   if ($post_title != "Database Error!"){
-                    while (!empty($post_title)){
+                    while (!$empty)){
                       $read = "1";
                       $id = $x;
                       echo "<a href='index.php?id=$x' rel='bookmark'> $post_title </a>";
