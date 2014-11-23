f2699524d3c7a81e98e9af4073af309587754409
MTRNord <mtrnord1@gmail.com>
2014-11-23 13:16:33 +0100

    fix endless loop

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -83,11 +83,13 @@
               <?php
                 $x = 1;
                 if ($empty){
+                  if ($post_title != "Database Error!"){
                     while (!empty($post_title)){
                       $id = $x;
                       echo "<a href='index.php?id=$x' rel='bookmark'> $post_title </a>";
                       $x = $x+1;
                     }
+                  }
                 }
               ?>
             </h1>
