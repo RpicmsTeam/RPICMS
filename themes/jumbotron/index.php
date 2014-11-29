9f8496d4978d6f1dd1f8faf703b711745f58f9d3
MTRNord <mtrnord1@gmail.com>
2014-11-29 17:53:52 +0100

    add redirect (mavbe correct)

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -90,7 +90,7 @@
             select.form.reset();
             parent.frames["unten"].focus();
           } else {
-            location.href = wert;
+            window.document.location.href = wert;
             }
         }
       }
