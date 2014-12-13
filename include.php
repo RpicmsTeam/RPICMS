e2f63b3063d1870cbea3b40313abd979de92b109
MTRNord <mtrnord1@gmail.com>
2014-12-13 19:32:34 +0100

    add missed characters

--- include.php
+++ include.php
@@ -1,7 +1,7 @@
 <?php
-ini_restore('include_path')
+ini_restore('include_path');
 if (get_include_path() = ini_restore('include_path') . getcwd()) {
-	ini_restore('include_path')
+	ini_restore('include_path');
 }else{
 	set_include_path(get_include_path() . PATH_SEPARATOR . getcwd());
 	echo get_include_path();
