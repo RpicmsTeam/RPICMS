389aac21d6d23e5a6bcbaff33addfcca7bde8c13
MTRNord <mtrnord1@gmail.com>
2014-12-13 19:31:16 +0100

    fix it

--- include.php
+++ include.php
@@ -1,4 +1,9 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . getcwd());
-echo get_include_path();
+ini_restore('include_path')
+if (get_include_path() = ini_restore('include_path') . getcwd()) {
+	ini_restore('include_path')
+}else{
+	set_include_path(get_include_path() . PATH_SEPARATOR . getcwd());
+	echo get_include_path();
+}
 ?>
