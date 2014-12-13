e1781d04ed18e1dec12fae4df33a1f11feba7cc4
MTRNord <mtrnord1@gmail.com>
2014-12-13 19:36:17 +0100

    cleanup include.php

--- include.php
+++ include.php
@@ -1,10 +1,5 @@
 <?php
-set_include_path(".:/usr/share/php:/usr/share/pear");
-#ini_restore('include_path');
-#if (get_include_path() = ini_restore('include_path') . getcwd()) {
-#	ini_restore('include_path');
-#}else{
-	set_include_path(get_include_path() . PATH_SEPARATOR . getcwd());
-	echo get_include_path();
-#}
+#set_include_path(".:/usr/share/php:/usr/share/pear");
+set_include_path(get_include_path() . PATH_SEPARATOR . getcwd());
+#echo get_include_path();
 ?>
