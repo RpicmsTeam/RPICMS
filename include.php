dafc9a84b6c0aa30f40dc6184d44c84e3a5cf2e3
MTRNord <mtrnord1@gmail.com>
2014-12-13 19:35:05 +0100

    manual rstoring of include_path

--- include.php
+++ include.php
@@ -1,5 +1,6 @@
 <?php
-ini_restore('include_path');
+set_include_path(".:/usr/share/php:/usr/share/pear");
+#ini_restore('include_path');
 #if (get_include_path() = ini_restore('include_path') . getcwd()) {
 #	ini_restore('include_path');
 #}else{
