471f19f5642691fde674fd59d46653a888c3bdec
MTRNord <mtrnord1@gmail.com>
2014-12-13 20:58:14 +0100

    again I failed

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -64,7 +64,13 @@
       }   
        // AND SO ON .................  
     }  
-
+###############################
+# include files from root dir #
+###############################
+$root_1 = realpath($_SERVER["DOCUMENT_ROOT"]);
+$currentdir = getcwd();
+$root_2 = str_replace($root_1, '', $currentdir);
+$root = explode("/", $root_2);
   ?>
 <!DOCTYPE html>
 <html lang="en">
