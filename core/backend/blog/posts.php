f325f11598676188655fbc46f17b180b80df0792
MTRNord <mtrnord1@gmail.com>
2014-12-14 11:03:13 +0100

    the if don't work correctly

--- core/backend/blog/posts.php
+++ core/backend/blog/posts.php
@@ -14,8 +14,7 @@ $root_1 = realpath($_SERVER["DOCUMENT_ROOT"]);
 $currentdir = getcwd();
 $root_2 = str_replace($root_1, '', $currentdir);
 $root_3 = explode("/", $root_2);
-if ($root_3[1] == 'core') {
-  echo $root_3[1];
+if ($root_3[1] == "core") {
   $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 }else{
   $root = $root_1 . '/' . $root_3[1];
