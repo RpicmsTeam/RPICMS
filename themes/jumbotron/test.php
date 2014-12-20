b5eebf5401e006855f6a60f22cf853efc152e572
MTRNord <mtrnord1@gmail.com>
2014-12-20 18:34:48 +0100

    bootstrap make problems :/

--- themes/jumbotron/test.php
+++ themes/jumbotron/test.php
@@ -7,17 +7,28 @@ header("Cache-Control: no-cache, must-revalidate, no-store");
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Bootstrap core CSS -->
+    <link href="../../core/libs/theme_engine/BootStrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">-->
+
+    <script src="../../core/libs/theme_engine/BootStrap/js/bootstrap.min.js"></script>
     <script src="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js"></script>
 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 <script type="text/javascript">
 	function showhide(id){
         if (document.getElementById) {
-          var divid = document.getElementById(id);
-          var divs = document.getElementsByClassName("hide");
+          var divid = document.getElementById(id).noConflict();
+          var divs = document.getElementsByClassName("hide").noConflict();
           for(var i=0;i<divs.length;i++) {
-            divs[i].style.display = "none";
+            divs[i].style.display.noConflict() = "none";
           }
-          divid.style.display = "block";
+          divid.style.display.noConflict() = "block";
         } 
         return false;
  	}
