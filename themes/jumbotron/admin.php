34cbae626c4a7483c994b35ab8ac95f8afee645c
MTRNord <mtrnord1@gmail.com>
2014-12-20 15:22:15 +0100

    cleanup code

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -127,10 +127,7 @@ foreach($langs as $prio => $lang) {
         <div role="tabpanel">
   			<!-- Nav tabs -->
   			<ul class="nav nav-tabs" role="tablist">
-   				<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-    			<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-    			<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-    			<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+    			<li role="presentation"><a href="#posts" aria-controls="posts" role="tab" data-toggle="tab">Posts</a></li>
   			</ul>
 		</div>
         <div id="navbar" class="navbar-collapse collapse">
@@ -153,10 +150,7 @@ foreach($langs as $prio => $lang) {
       	<h1>Administration</h1>
   		<!-- Tab panes -->
   		<div class="tab-content">
-    		<div role="tabpanel" class="tab-pane active" id="home">...</div>
-    		<div role="tabpanel" class="tab-pane" id="profile">...</div>
-    		<div role="tabpanel" class="tab-pane" id="messages">...</div>
-    		<div role="tabpanel" class="tab-pane" id="settings">...</div>
+    		<div role="tabpanel" class="tab-pane" id="posts">1</div>
   		</div>
       </div>
     </div>
