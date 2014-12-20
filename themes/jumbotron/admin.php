db2efc7a5a09e951c6b1e4741b4809b6e2a6d3b8
MTRNord <mtrnord1@gmail.com>
2014-12-20 19:35:35 +0100

    found the bug

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -124,7 +124,13 @@ foreach($langs as $prio => $lang) {
 	<script type="text/javascript" src="../../core/libs/theme_engine/tinymce/tinymce.min.js"></script>
 	<script type="text/javascript">
 	tinymce.init({
-    	selector: "textarea"
+    	selector: "textarea",
+    	plugins: [
+        	"advlist autolink lists link image charmap print preview anchor",
+        	"searchreplace visualblocks code fullscreen",
+        	"insertdatetime media table contextmenu paste"
+    	],
+    	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
 	});
 	</script>
   </head>
