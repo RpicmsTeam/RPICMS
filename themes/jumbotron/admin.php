b1490fd391bbaaa52ff377a121da36ae17259e94
MTRNord <mtrnord1@gmail.com>
2014-12-20 15:16:50 +0100

    tabs

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -103,6 +103,12 @@ foreach($langs as $prio => $lang) {
         }
       }
     </script>
+    <script type="text/javascript">
+    	$('#myTab a').click(function (e) {
+  			e.preventDefault()
+  			$(this).tab('show')
+		})
+    </script>
   </head>
   <body>
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -118,6 +124,25 @@ foreach($langs as $prio => $lang) {
           echo '<a class="navbar-brand" href="index.php">'.$blog_name.'</a>';
           ?>
         </div>
+        <div role="tabpanel">
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">...</div>
+    <div role="tabpanel" class="tab-pane" id="profile">...</div>
+    <div role="tabpanel" class="tab-pane" id="messages">...</div>
+    <div role="tabpanel" class="tab-pane" id="settings">...</div>
+  </div>
+
+</div>
         <div id="navbar" class="navbar-collapse collapse">
         <form class="navbar-form navbar-right" role="form" action="../../core/backend/admin/user/login_check.php" method="post">
             <div class="form-group">
