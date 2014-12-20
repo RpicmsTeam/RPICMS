6ae920820ef7344ba3f8342121072220d9608634
MTRNord <mtrnord1@gmail.com>
2014-12-20 19:40:50 +0100

    now it work :D

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -129,8 +129,7 @@ foreach($langs as $prio => $lang) {
         	"advlist autolink lists link image charmap print preview anchor",
         	"searchreplace visualblocks code fullscreen",
         	"insertdatetime media table contextmenu paste"
-    	],
-    	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    	]
 	});
 	</script>
   </head>
