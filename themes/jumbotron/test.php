aea63ba604d647b5a32c15a54579c7754c31dd14
MTRNord <mtrnord1@gmail.com>
2014-12-20 18:39:22 +0100

    a few commits left befor I can fix it ;)

--- themes/jumbotron/test.php
+++ themes/jumbotron/test.php
@@ -9,10 +9,10 @@ header("Cache-Control: no-cache, must-revalidate, no-store");
 <head>
     <!-- Bootstrap core CSS -->
     <link href="../../core/libs/theme_engine/BootStrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="jumbotron.css" rel="stylesheet">
+    <!-- Custom styles for this template 
+    <link href="jumbotron.css" rel="stylesheet">-->
 
-    <!--<script src="../../core/libs/theme_engine/BootStrap/js/bootstrap.min.js"></script>-->
+    <script src="../../core/libs/theme_engine/BootStrap/js/bootstrap.min.js"></script>
     <script src="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
