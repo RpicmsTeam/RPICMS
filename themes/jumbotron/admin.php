7c6a502fad73799b1f33f2a35b4cd6d8d9922fd8
MTRNord <mtrnord1@gmail.com>
2014-12-20 19:27:33 +0100

    add tinymce

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -121,6 +121,18 @@ foreach($langs as $prio => $lang) {
 		}
 		onload = foo;
 	</script>
+	<script type="text/javascript" src="../../core/libs/theme_engine/tinymce/tinymce.min.js"></script>
+	<script type="text/javascript">
+	tinymce.init({
+    	selector: "textarea",
+    	plugins: [
+        	"advlist autolink lists link image charmap print preview anchor",
+        	"searchreplace visualblocks code fullscreen",
+        	"insertdatetime media table contextmenu paste moxiemanager"
+    	],
+    	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+	});
+	</script>
   </head>
   <body>
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
