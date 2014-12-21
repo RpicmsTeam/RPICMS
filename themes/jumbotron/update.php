9ef95a0912ae8d91bd649e0da039c089e553a053
MTRNord <mtrnord1@gmail.com>
2014-12-21 08:54:27 +0100

    update updater :D

--- themes/jumbotron/update.php
+++ themes/jumbotron/update.php
@@ -52,11 +52,42 @@ if ($latest !== false) {
 else {
 	echo $update->getLastError();
 }
-// dauerhafte PHP-Weiterleitung (Statuscode 301)
-header("HTTP/1.1 301 Moved Permanently");
-// Weiterleitungsziel. Wohin soll eine permanente Weiterleitung erfolgen?
-header("Location:admin.php");
-// zur Sicherheit ein exit-Aufruf, falls Probleme aufgetreten sind
-exit;
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+    
+		<link rel="icon" href="../../core/libs/theme_engine/BootStrap/favicon.ico">
+		<?php
+			echo "<title>$blog_name</title>";
+		?>
+
+		<!-- Bootstrap core CSS -->
+		<link href="../../core/libs/theme_engine/BootStrap/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="jumbotron.css" rel="stylesheet">
+
+		<script src="../../core/libs/theme_engine/BootStrap/js/bootstrap.min.js"></script>
+		<script src="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js"></script>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<form action="admin.php">
+    		<button type="submit" value="Zurück">Zurück</button>
+		</form>
+	</body>
+</html>
+<?php
 }
 ?>
