8b9577ca655db665d11945471f50a3832ca8024c
MTRNord <mtrnord1@gmail.com>
2014-12-30 23:35:28 +0100

    it will be rewriten ;)

--- core/backend/admin/user/login/login.php
+++ core/backend/admin/user/login/login.php
@@ -1,6 +1,12 @@
+<head>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" ></script>
 <script type="text/javascript">
+	function loadquery(){
 		var login_module = "modul_g-plus-login";
     	$('#DisplayDiv').load("../../modules/" + login_module + "/login.pho" + email_result);
+    }
 </script>
+</head>
+<body onload="loadquery();">
 <div id="DisplayDiv"></div>
+</body>
