6789e89e39b38ee77e241897c035706ece9744ba
MTRNord <mtrnord1@gmail.com>
2015-01-02 17:11:03 +0100

    mobile fixes

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -82,7 +82,7 @@ if ($root_3[1] == 'core') {
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="">
     
