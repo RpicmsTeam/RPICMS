e4472729a0b788b72658de23741b15e32961b33a
MTRNord <mtrnord1@gmail.com>
2015-01-02 20:48:39 +0100

    It's a script xD

--- core/backend/admin/modules/html_header.php
+++ core/backend/admin/modules/html_header.php
@@ -1,6 +1,5 @@
   <script type="text/javascript" src="../../core/backend/admin/modules/share_buttons/socialshareprivacy/js/jquery.socialshareprivacy.js"></script>
-  <link href="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js" rel="stylesheet">
-  core/libs/theme_engine/jquery
+   <script type="text/javascript" src="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js"></script>
   <script type="text/javascript">
     jQuery(document).ready(function($){
       if($('#socialshareprivacy').length > 0){
