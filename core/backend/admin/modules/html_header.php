ab605175f6de1a5d96321a3d19fe75cfe68c1bc5
MTRNord <mtrnord1@gmail.com>
2015-01-02 20:35:40 +0100

    add module share_buttons

---
+++ core/backend/admin/modules/html_header.php
@@ -0,0 +1,12 @@
+  <script type="text/javascript" src="../../core/backend/admin/modules/share_buttons/socialshareprivacy/js/jquery.socialshareprivacy.js"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($){
+      if($('#socialshareprivacy').length > 0){
+        $('#socialshareprivacy').socialSharePrivacy({
+          "css_path"  : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy/socialshareprivacy.css",
+          "lang_path" : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy/lang/",
+          "language"  : "de"
+        });
+      }
+    });
+  </script>
