0d4b3048b6a63696bc417cb12e503edd0f310b56
MTRNord <mtrnord1@gmail.com>
2015-01-02 20:57:26 +0100

    update embading of share buttons

--- core/backend/admin/modules/html_header.php
+++ core/backend/admin/modules/html_header.php
@@ -1,24 +1,25 @@
-  <script type="text/javascript" src="../../core/backend/admin/modules/share_buttons/socialshareprivacy/js/jquery.socialshareprivacy.js"></script>
-   <script type="text/javascript" src="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js"></script>
-  <script type="text/javascript">
-    jQuery(document).ready(function($){
-      if($('#socialshareprivacy').length > 0){
-        $('#socialshareprivacy').socialSharePrivacy({
-          services : {
-            facebook : {
-              'perma_option' : 'off'
-            }, 
-            twitter : {
-              'status' : 'off'
-            },
-            gplus : {
-              'dummy_img' : '../../core/backend/admin/modules/share_buttons/socialshareprivacy/images/dummy_gplus_variante.png'
-            }
-          },
-          "css_path"  : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/socialshareprivacy.css",
-          "lang_path" : "../../core/backend/admin/modules/share_buttons/socialshareprivacy/lang/",
-          "language"  : "de"
-        });
-      }
-    });
-  </script>
+<?php 
+###############################
+# include files from root dir #
+###############################
+$root_1 = realpath($_SERVER["DOCUMENT_ROOT"]);
+$currentdir = getcwd();
+$root_2 = str_replace($root_1, '', $currentdir);
+$root_3 = explode("/", $root_2);
+if ($root_3[1] == 'core') {
+  echo $root_3[1];
+  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+}else{
+  $root = $root_1 . '/' . $root_3[1];
+}
+
+
+
+
+
+  $share = "share_buttons";//need to get extra config for modules
+  include($root.'/core/backend/admin/modules/'.$share. '/html_header.php');
+
+  
+
+ ?>
