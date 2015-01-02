774534a15ccb1e68800ad3bcc77b14464d136c5f
MTRNord <mtrnord1@gmail.com>
2015-01-02 20:13:39 +0100

    move modul integration

---
+++ core/backend/admin/modules/navigation.php
@@ -0,0 +1,25 @@
+<?php 
+###############################
+# include files from root dir #
+###############################
+$root_1 = realpath($_SERVER["DOCUMENT_ROOT"]);
+$currentdir = getcwd();
+$root_2 = str_replace($root_1, '', $currentdir);
+$root_3 = explode("/", $root_2);
+if ($root_3[1] == 'core') {
+  echo $root_3[1];
+  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+}else{
+  $root = $root_1 . '/' . $root_3[1];
+}
+
+
+
+
+
+	$login_module = "modul_g-plus-login";//need to get extra config for modules
+	include($root.'/core/backend/admin/modules/'.$login_module. '/index.php');
+
+	
+
+ ?>
