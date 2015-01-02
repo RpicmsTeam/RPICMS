6c6689cd8ebc50723d530bf14a79be39b3bc0f2a
MTRNord <mtrnord1@gmail.com>
2015-01-02 20:46:51 +0100

    try to fix jquery problem

--- core/backend/admin/modules/html_header.php
+++ core/backend/admin/modules/html_header.php
@@ -1,4 +1,6 @@
   <script type="text/javascript" src="../../core/backend/admin/modules/share_buttons/socialshareprivacy/js/jquery.socialshareprivacy.js"></script>
+  <link href="../../core/libs/theme_engine/jquery/jquery-1.11.2.min.js" rel="stylesheet">
+  core/libs/theme_engine/jquery
   <script type="text/javascript">
     jQuery(document).ready(function($){
       if($('#socialshareprivacy').length > 0){
