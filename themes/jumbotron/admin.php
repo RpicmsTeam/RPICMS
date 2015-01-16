708814ce841d2efd42b81a0051244f4f11107c61
MTRNord <mtrnord1@gmail.com>
2015-01-16 20:24:13 +0100

    add missing libs

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -1,4 +1,18 @@
-<?php 
+<?php
+
+include($root.'/core/libs/OAuth2/vendor/autoload.php');
+use fkooman\OAuth\Client\Api;
+use fkooman\OAuth\Client\Context;
+use fkooman\OAuth\Client\GoogleClientConfig;
+use fkooman\OAuth\Client\SessionStorage;
+
+$googleClientConfig = new GoogleClientConfig(
+    json_decode(file_get_contents($root.'/core/backend/admin/modules/modul_g-plus-login/client_secrets.json'), true)
+);
+$api = new Api("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
+$context = new Context("mtrnord1@gmail.com", array("https://www.googleapis.com/auth/plus.login"));
+$accessToken = $api->getAccessToken($context);
+
 if(isset($_COOKIE['PHPSESSID']) || $_COOKIE['PHPSESSID'] == $accessToken) {
 /**
 * Jumbotron Theme for RPICMS
