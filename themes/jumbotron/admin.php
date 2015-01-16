ae472a845fbba041e76551b9d5c607e8bb46bd14
MTRNord <mtrnord1@gmail.com>
2015-01-16 20:32:39 +0100

    new idea :D

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -19,21 +19,10 @@ if ($root_3[1] == 'core') {
   $root = $root_1 . '/' . $root_3[1];
 }
 
-include($root . '/core/libs/OAuth2/vendor/autoload.php');
-use fkooman\OAuth\Client\Api;
-use fkooman\OAuth\Client\Context;
-use fkooman\OAuth\Client\GoogleClientConfig;
-use fkooman\OAuth\Client\SessionStorage;
+$user = "test";
+$user_allowed = "test";
 
-$googleClientConfig = new GoogleClientConfig(
-    json_decode(file_get_contents($root.'/core/backend/admin/modules/modul_g-plus-login/client_secrets.json'), true)
-);
-$api = new Api("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
-$context = new Context("mtrnord1@gmail.com", array("https://www.googleapis.com/auth/plus.login"));
-$accessToken = $api->getAccessToken($context);
-
-
-if(isset($_COOKIE['PHPSESSID']) || $_COOKIE['PHPSESSID'] == $accessToken) {
+if($user_allowed = true) {
 #######################
 # flush browser cache #
 #######################
