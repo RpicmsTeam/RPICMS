9a1dc6cef413fa4dab516edf016ae74ebe08865c
MTRNord <mtrnord1@gmail.com>
2015-02-02 20:07:27 +0100

    not complete working :/

--- core/api/v1/api.php
+++ core/api/v1/api.php
@@ -34,7 +34,8 @@ class MyAPI extends API
             return "Only accepts GET requests";
         }
      }
-     // Requests from the same server don't have a HTTP_ORIGIN header
+    // Requests from the same server don't have a HTTP_ORIGIN header
+    
     if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
         $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
     }
