23a08332ef2f740a26e9c805d0fccfe28567cafa
MTRNord <mtrnord1@gmail.com>
2015-02-02 20:14:13 +0100

    I'm so genius....

--- core/api/v1/api.php
+++ core/api/v1/api.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'MyApi.class.php';
+require_once 'api.class.php';
 // Requests from the same server don't have a HTTP_ORIGIN header
 if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
