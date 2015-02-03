20b4fa4f1d22fdc7f30694a06dbd74e5fcfb99fe
MTRNord <mtrnord1@gmail.com>
2015-02-03 12:56:59 +0100

    commit wasn't correct :O

--- api.php
+++ api.php
@@ -11,8 +11,6 @@ if (!file_exists('core/config/connect.db.inc.php')) {
 			$Api = substr($request, strrpos($request, 'v1/') + 1);
 			echo $request;
 			echo "</br>";
-			echo $pos;
-			echo "</br>";
 			echo $Api;
 			//header("HTTP/1.1 301 Moved Permanently");
 			//header("Location:api/v1/api.php/$Api");
