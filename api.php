f59a303fd8ddcf9cf1ccf751d905157da639249a
MTRNord <mtrnord1@gmail.com>
2015-02-03 12:16:46 +0100

    try to workaround the Api problems

---
+++ api.php
@@ -0,0 +1,19 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate, no-store");
+//$root = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+echo $root;
+$check = !file_exists('core/config/connect.db.inc.php');
+echo $check;
+if (!file_exists('core/config/connect.db.inc.php')) {
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location:".$root."install/index.php");
+	}else{
+			$request = $_SERVER['REQUEST_URI'];
+			if (($pos = strpos($request, "v1/")) !== FALSE) { 
+    			$Api = substr($data, $pos+1); 
+			}
+			header("HTTP/1.1 301 Moved Permanently");
+			header("Location:api/v1/api.php/$Api");
+	}
+
+?>
