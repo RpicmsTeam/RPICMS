156c50e583877031faab5cf148fe5eb9d4937097
MTRNord <mtrnord1@gmail.com>
2015-02-03 13:00:40 +0100

    reactivate forwarding

--- api.php
+++ api.php
@@ -9,11 +9,8 @@ if (!file_exists('core/config/connect.db.inc.php')) {
 	}else{
 			$request = $_SERVER['REQUEST_URI'];
 			$Api = substr($request, strrpos($request, 'v1/') + 3);
-			echo $request;
-			echo "</br>";
-			echo $Api;
-			//header("HTTP/1.1 301 Moved Permanently");
-			//header("Location:api/v1/api.php/$Api");
+			header("HTTP/1.1 301 Moved Permanently");
+			header("Location:api/v1/api.php/$Api");
 	}
 
 ?>
