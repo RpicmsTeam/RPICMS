f713c8e33415d2faa910eec0536b7ac1241c4f54
MTRNord <mtrnord1@gmail.com>
2015-02-03 12:47:04 +0100

    :/

--- api.php
+++ api.php
@@ -12,7 +12,9 @@ if (!file_exists('core/config/connect.db.inc.php')) {
     			$Api = substr($request, $pos+1); 
 			}
 			echo $request;
+			echo "</br>";
 			echo $pos;
+			echo "</br>";
 			echo $Api;
 			//header("HTTP/1.1 301 Moved Permanently");
 			//header("Location:api/v1/api.php/$Api");
