e156c2a1453ddd7efc3c36f333c06ecae2a12d4e
MTRNord <mtrnord1@gmail.com>
2015-02-22 19:13:19 +0100

    test push

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ git add . --all
 echo "Commit:"
 read commit
 git commit -m "$commit"
-git remote add origin https://github.com/RpicmsTeam/RPICMS.git
+git remote add origin https://mtrnord:b5b2678ba4947697251a3e9c5235442283adb440@github.com/RpicmsTeam/RPICMS.git
 git push -f -u origin develop
 git remote remove origin
 echo "Fertig!"
