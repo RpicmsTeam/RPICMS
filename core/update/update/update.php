9f2dde11dd72efa007c380d95749d846734d5ef8
MTRNord <mtrnord1@gmail.com>
2015-02-23 20:27:53 +0100

    this could work

--- core/update/update/update.php
+++ core/update/update/update.php
@@ -78,7 +78,7 @@ class AutoUpdate
 	 *
 	 * @var string
 	 */
-   private $_updateFile = "update.json".".".$this->_branch;
+   private $_updateFile = "update.json.".$this->{_branch};
 
 	/**
 	 * Current version.
