1c5c640a16cf1f197f8d0a95a488bb30bce619c5
MTRNord <mtrnord1@gmail.com>
2015-02-28 22:04:23 +0100

    lol theres a buggy fiels

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -224,7 +224,7 @@ foreach($langs as $prio => $lang) {
                 </tr>
                   <?php
                     include($root . '/core/backend/blog/posts.php');
-                    while ($x < $post_id_clean+1){
+                    while ($x < $post_id_clean){
                       include($root . '/core/config/connect.db.inc.php');
                       echo "<tr>";
                       echo "<td>$id</td>";
