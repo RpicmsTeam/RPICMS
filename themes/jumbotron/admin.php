1cad024295134e1d5c84500d0d6e37fcb3eb40b0
MTRNord <mtrnord1@gmail.com>
2015-02-28 22:47:14 +0100

    ehmm arrow was wrong xD

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -234,7 +234,7 @@ foreach($langs as $prio => $lang) {
                       echo "<td>94</td>";
                       echo "</tr>";
                       $x = $x+1;
-                      if($x < 1){
+                      if($x > 1){
                         $id = $id+1;
                       }else{
                         $id = $id+2;
