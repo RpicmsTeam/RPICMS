a362e27ff9e768135f386fef441a2fd769792d21
MTRNord <mtrnord1@gmail.com>
2015-03-17 18:03:56 +0100

    same error here

--- core/api/v1/DbConnect.class.php
+++ core/api/v1/DbConnect.class.php
@@ -19,31 +19,32 @@ if ($root_3[1] == 'core') {
  * @author Ravi Tamada
  */
 class DbConnect {
- 
+
     private $conn;
- 
-    function __construct() {        
+
+    function __construct() {
     }
- 
+
     /**
      * Establishing database connection
      * @return database connection handler
      */
     function connect() {
+        global $root;
         include_once $root . '/core/config/api.php';
- 
+
         // Connecting to mysql database
         $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
         // Check for database connection error
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
- 
+
         // returing connection resource
         return $this->conn;
     }
- 
+
 }
- 
+
 ?>
