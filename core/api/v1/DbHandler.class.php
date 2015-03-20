3da821c44b50749cb5f9f67fb795220d0c09c400
MTRNord <mtrnord1@gmail.com>
2015-03-20 16:46:39 +0100

    all the time xD

--- core/api/v1/DbHandler.class.php
+++ core/api/v1/DbHandler.class.php
@@ -241,7 +241,7 @@ class DbHandler {
      */
     public function getPosts($post_id) {
       $id = $this->conn->prepare("SELECT id FROM posts");
-      echo $id;
+      var_dump($id);
       /*while ($id->execute()) {
    				$post_id_clean_array = $id->get_result()->fetch_assoc();
           $post_id_clean = $post_id_clean_array["id"];
