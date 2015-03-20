11c5be11325a4d9528c18cb36065d8bd3fdb4cd7
MTRNord <mtrnord1@gmail.com>
2015-03-20 17:15:00 +0100

    another try to get this work

--- core/api/v1/DbHandler.class.php
+++ core/api/v1/DbHandler.class.php
@@ -247,7 +247,7 @@ class DbHandler {
       }
       if ($post_id == NULL) {
         $x = 1;
-        var_dump($post_id_clean_array["id"]);
+        var_dump($post_id_clean_array);
         while ($x < $post_id_clean+1){
           $stmt = $this->conn->prepare("SELECT id,title,text,author,category,date FROM posts WHERE id = ?");
           $stmt->bind_param("i", $id);
