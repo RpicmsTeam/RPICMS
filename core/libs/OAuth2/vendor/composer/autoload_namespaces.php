92d99a9a4272f074700b1a11eaab374b61ff8667
MTRNord <mtrnord1@gmail.com>
2015-04-01 20:37:48 +0200

    fix lib

--- core/libs/OAuth2/vendor/composer/autoload_namespaces.php
+++ core/libs/OAuth2/vendor/composer/autoload_namespaces.php
@@ -8,7 +8,4 @@ $baseDir = dirname($vendorDir);
 return array(
     'fkooman\\OAuth\\Common\\' => array($vendorDir . '/fkooman/oauth-common/src'),
     'fkooman\\OAuth\\Client' => array($baseDir . '/src'),
-    'GuzzleHttp\\Stream\\' => array($vendorDir . '/guzzlehttp/streams/src'),
-    'GuzzleHttp\\Ring\\' => array($vendorDir . '/guzzlehttp/ringphp/src'),
-    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
 );
