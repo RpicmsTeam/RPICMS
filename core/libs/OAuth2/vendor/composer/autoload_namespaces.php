1aa3cf950969bcc2fd0c99efae37d2b2df415260
MTRNord <mtrnord1@gmail.com>
2015-04-01 20:30:55 +0200

    reupdate lib

--- core/libs/OAuth2/vendor/composer/autoload_namespaces.php
+++ core/libs/OAuth2/vendor/composer/autoload_namespaces.php
@@ -8,6 +8,6 @@ $baseDir = dirname($vendorDir);
 return array(
     'fkooman\\OAuth\\Common\\' => array($vendorDir . '/fkooman/oauth-common/src'),
     'fkooman\\OAuth\\Client' => array($baseDir . '/src'),
-    'GuzzleHttp\\Client\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
-
+    'GuzzleHttp\\Tests' => array($vendorDir . '/guzzlehttp/guzzle/tests'),
+    'GuzzleHttp' => array($vendorDir . '/guzzlehttp/guzzle/src'),
 );
