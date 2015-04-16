08f548472f25ac9759dca023b7db900045bb9fd7
MTRNord <mtrnord1@gmail.com>
2015-04-16 17:04:58 +0200

    add menu Items

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -149,18 +149,6 @@ foreach($langs as $prio => $lang) {
 		}
 		onload = foo;
 	</script>
-	<script type="text/javascript" src="../../core/libs/theme_engine/tinymce/tinymce.min.js"></script>
-	<!--<script type="text/javascript">
-	tinymce.init({
-    	selector: "textarea",
-    	plugins: [
-        	"advlist autolink lists link image charmap print preview anchor",
-        	"searchreplace visualblocks code fullscreen",
-        	"insertdatetime media table contextmenu paste"
-    	],
-    	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-	});
-	</script>-->
   </head>
   <body>
     <nav id="navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
