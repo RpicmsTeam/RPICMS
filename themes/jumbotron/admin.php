6c6f784e992c584d39f3af46a94267365a5ab16d
MTRNord <mtrnord1@gmail.com>
2015-04-16 16:48:29 +0200

    now?

--- themes/jumbotron/admin.php
+++ themes/jumbotron/admin.php
@@ -163,8 +163,8 @@ foreach($langs as $prio => $lang) {
 	</script>-->
   </head>
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container" style="overflow-x:auto; overflow-y:auto;">
+    <nav class="navbar navbar-inverse navbar-fixed-top navbar-collapse collapse" role="navigation">
+      <div class="container">
         <div class="navbar-header" style="float:left;">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
@@ -175,7 +175,6 @@ foreach($langs as $prio => $lang) {
           <?php
           echo '<a class="navbar-brand" href="index.php">'.$blog_name.'</a> ';
           ?>
-          <div id="navbar" class="navbar-collapse collapse" style="float:right;">
             <ul class="nav navbar-nav">
               <li class="dropdown">
                 <a href="#settings" class="dropdown-toggle active disabled" data-toggle="dropdown" role="button" aria-expanded="false" onclick="showhide('settings');">Settings <span class="sr-only">(current)</span></a>
@@ -190,7 +189,6 @@ foreach($langs as $prio => $lang) {
                 </ul>
               </li>
             </ul>
-          </div><!--/.navbar-collapse -->
         </div>
       </div>
     </nav>
