8445a516c516b01e0d171d5c20df97e05bd93d33
MTRNord <mtrnord1@gmail.com>
2015-04-17 09:01:21 +0200

    fix link

--- themes/jumbotron/admin_pages/user_pages.php
+++ themes/jumbotron/admin_pages/user_pages.php
@@ -46,7 +46,7 @@ foreach ($dbpages as $page){
 	".$page['id']."
 	</td>
 	<td>
-	<a onclick=\"showhide('usercenter');\" href='admin.php#usercenter?page_id=".$page['id']."'>".$page['page']."</a>
+	<a onclick=\"showhide('usercenter');\" href='admin.php?page_id=".$page['id']."#usercenter'>".$page['page']."</a>
 	</td>
 	<td>";
 
