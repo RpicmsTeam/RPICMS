2d0214ccc33fcf2934b05e40e18fd9e7c0b3d106
MTRNord <mtrnord1@gmail.com>
2015-04-19 13:55:31 +0200

    add bootstrap

--- install/index.php
+++ install/index.php
@@ -9,6 +9,12 @@
 <html>
 <head>
 	<title>Installer | Check for requirements</title>
+	<!-- Bootstrap core CSS -->
+	<link href="../core/libs/theme_engine/BootStrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="../themes/jumbotron/jumbotron.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../core/libs/theme_engine/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../core/libs/theme_engine/bootstrap-social/bootstrap-social.min.css">
 </head>
 <body>
 <h1>Check for requirements</h1>
@@ -63,5 +69,10 @@ if ($mysql == 1 && $dir == 1){
 	echo "Can't proceed!</br>";
 }
 ?>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="../core/libs/theme_engine/BootStrap/js/bootstrap.min.js"></script>
 </body>
 </html>
