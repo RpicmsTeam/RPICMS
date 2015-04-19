3f01441d47d47c5ba423a17b8cb0b26d2fa2db4c
MTRNord <mtrnord1@gmail.com>
2015-04-19 15:30:19 +0200

    add $root

--- install/set_variables.php
+++ install/set_variables.php
@@ -6,6 +6,21 @@
 * @author	Marcel Radzio <info@nordgedanken.de>
 * @version	0.2 18/08/2014 18:45
 */-->
+<?php
+  ###############################
+  # include files from root dir #
+  ###############################
+  $root_1 = realpath($_SERVER["DOCUMENT_ROOT"]);
+  $currentdir = getcwd();
+  $root_2 = str_replace($root_1, '', $currentdir);
+  $root_3 = explode("/", $root_2);
+  if ($root_3[1] == 'core') {
+    echo $root_3[1];
+    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+  }else{
+    $root = $root_1 . '/' . $root_3[1];
+  }
+?>
 <html>
 <head>
   <title>Installer | Set Variables</title>
