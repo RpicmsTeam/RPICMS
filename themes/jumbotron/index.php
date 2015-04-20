c28966e2d42bef578042c7d624c3f976321c21ca
MTRNord <mtrnord1@gmail.com>
2015-04-20 17:10:02 +0200

    load jquery local

--- themes/jumbotron/index.php
+++ themes/jumbotron/index.php
@@ -353,7 +353,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="../../core/libs/theme_engine/jquery2.1.3.min.js"></script>
     <script src="../../core/libs/theme_engine/BootStrap/js/bootstrap.min.js"></script>
     <?php include($root . '/core/backend/admin/modules/script_footer.php'); ?>
   </body>
