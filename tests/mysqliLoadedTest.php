be26449202674e6315c594f3ddff60300a65084d
mtrnord <mtrnord1@gmail.com>
2015-10-04 20:18:44 +0200

    try to fix output

--- tests/mysqliLoadedTest.php
+++ tests/mysqliLoadedTest.php
@@ -8,25 +8,25 @@ class mysqliLoadedTest extends PHPUnit_Framework_TestCase
         }else{
             fwrite(STDERR, print_r('[mysqliLoadedTest]->[testForMysqli()]: The MySQLi extension is available', TRUE));
         }
-        return 0;
     }
 
     /**
-    * @depends testForMysqli
-    */
+     * @depends testForMysqli
+     */
     public function tryDBConnection()
     {
       $mysqli = new mysqli("localhost","root","","RPICMS");
-
       // Check connection
       if ($mysqli->connect_errno > 0)
       {
         $this->assertFalse($mysqli->connect_error());
         fwrite(STDERR, print_r('[mysqliLoadedTest]->[tryDBConnection()]: Failed to connect to MySQLi: ' . $mysqli->connect_error(), TRUE));
       }else{
-        fwrite(STDERR, print_r('[mysqliLoadedTest]->[tryDBConnection()]: Could connect to MySQLi: ', TRUE));
+        $this->expectOutputString('[mysqliLoadedTest]->[tryDBConnection()]: Could connect to MySQLi');
+        print '[mysqliLoadedTest]->[tryDBConnection()]: Could connect to MySQLi';
+        #fwrite(STDERR, print_r('[mysqliLoadedTest]->[tryDBConnection()]: Could connect to MySQLi', TRUE));
       }
-      return 0;
+      return "test";
     }
 }
 ?>
