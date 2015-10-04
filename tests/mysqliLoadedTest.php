7f451a92ad84f862a42322e0e50793bbeca84a32
mtrnord <mtrnord1@gmail.com>
2015-10-04 20:11:42 +0200

    Add new Output to Test

--- tests/mysqliLoadedTest.php
+++ tests/mysqliLoadedTest.php
@@ -19,7 +19,9 @@ class mysqliLoadedTest extends PHPUnit_Framework_TestCase
       if ($mysqli->connect_errno > 0)
       {
         $this->assertFalse($mysqli->connect_error());
-        fwrite(STDERR, print_r('[mysqliLoadedTest]->[testForMysqli()]: Failed to connect to MySQL: ' . $mysqli->connect_error(), TRUE));
+        fwrite(STDERR, print_r('[mysqliLoadedTest]->[tryDBConnection()]: Failed to connect to MySQLi: ' . $mysqli->connect_error(), TRUE));
+      }else{
+        fwrite(STDERR, print_r('[mysqliLoadedTest]->[tryDBConnection()]: Could connect to MySQLi: ', TRUE));
       }
 
     }
