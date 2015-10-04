83e2138aacacef0e2d01dd10b59056df51d4aafa
Marcel <MTRNord@users.noreply.github.com>
2015-10-04 15:33:19 +0200

    Create mysqliLoadedTest.php

---
+++ Tests/mysqliLoadedTest.php
@@ -0,0 +1,13 @@
+<?php
+class mysqliLoadedTest extends PHPUnit_Framework_TestCase
+{
+    public function testForMysqli()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped('[mysqliLoadedTest]->[testForMysqli()]: The MySQLi extension is not available');
+        }else{
+            fwrite(STDERR, print_r('[mysqliLoadedTest]->[testForMysqli()]: The MySQLi extension is available', TRUE));
+        }
+    }
+}
+?>
